dda8ebe3f38ac0cd322993401b7f454085a75c67
mago <markus.schneider84@gmail.com>
2021-08-07 09:37:11 +0200

    test

---
+++ Art/Backgrounds/cloudsset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="cloudsset" tilewidth="16" tileheight="16" tilecount="1600" columns="40">
+ <image source="cloudsset.png" width="640" height="640"/>
+</tileset>
